d6f7b72b2297cc719ede6cebbc3e7e90851bb22f
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-11-19 14:16:36 -0300

    Implementing complex scenarios

--- src/main/java/com/implementation/cache/Service/PiService.java
+++ src/main/java/com/implementation/cache/Service/PiService.java
@@ -4,6 +4,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Component
 public class PiService {
@@ -14,8 +20,47 @@ public class PiService {
         this.cachedPiService = cachedPiService;
     }
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 2500)
     public void printPi() {
-        System.out.println("Pi: " + cachedPiService.computePi(1500));
+
+        List<Integer> toCompute = Arrays.asList(100, 1000, 1500, 900, 750);
+
+        // Benchmark Linear Execution
+        //Duration linearTime = benchmarkLinear(toCompute);
+        //System.out.println("Linear Benchmark: " + linearTime.toMillis() + " ms");
+
+        // Benchmark Parallel Execution
+        Duration parallelTime = benchmarkParallel(toCompute);
+        System.out.println("Parallel Benchmark: " + parallelTime.toMillis() + " ms");
+    }
+
+    private Duration benchmarkLinear(List<Integer> toCompute) {
+        Instant beginTime = Instant.now();
+
+        // Linear programming
+        toCompute.forEach(e -> {
+            BigDecimal result = cachedPiService.computePi(e);
+            System.out.println("Pi (linear): " + result);
+        });
+
+        return Duration.between(beginTime, Instant.now());
+    }
+
+    private Duration benchmarkParallel(List<Integer> toCompute) {
+        Instant beginTime = Instant.now();
+
+        // Parallel programming
+        List<CompletableFuture<BigDecimal>> futures = toCompute.stream()
+                .map(e -> CompletableFuture.supplyAsync(() -> cachedPiService.computePi(e)))
+                .collect(Collectors.toList());
+
+        List<BigDecimal> results = futures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        // Print results
+        results.forEach(result -> System.out.println("Pi (parallel): " + result));
+
+        return Duration.between(beginTime, Instant.now());
     }
 }
